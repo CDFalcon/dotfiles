ec55900e7a0d5f2015d0febcd65f5827fcb6e208
Colby Duke <colby.duke@bellsouth.net>
2022-06-23 12:53:22 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/.vim/bundle
+git clone https://github.com/tpope/vim-fugitive
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/airblade/vim-gitgutter
+git clone https://github.com/w0rp/ale
